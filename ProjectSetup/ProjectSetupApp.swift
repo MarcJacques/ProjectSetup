62367712a2075e594695cd4bb62102c99865751c
Marc <marcjacques@me.com>
2022-01-01 10:41:20 -0500

    Initial Commit

---
+++ ProjectSetup/ProjectSetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSetupApp.swift
+//  ProjectSetup
+//
+//  Created by Marc Jacques on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
